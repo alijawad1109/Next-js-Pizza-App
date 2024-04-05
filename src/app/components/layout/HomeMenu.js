de14781fc88a3844fa15551747dba5f420f5e762
shampigoli <alijawad1109@gmail.com>
2024-04-05 14:34:35 +0500

    ajajaj

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -27,7 +27,7 @@ const HomeMenu = () => {
       <SectionHeader subHeader={"Check Out"} mainHeader={"Our Best Sellers"} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {bestSellers?.length > 0 &&
-          bestSellers.map((item) => <MenuItems key={item.id} {...item} />)}
+          bestSellers.map((item) => <MenuItems key={item._id} {...item} />)}
       </div>
     </section>
   );
